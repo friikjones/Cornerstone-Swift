d027146cb4a75084f8527a42c76c5ff363370e8f
friikjones <d.mullerjones@gmail.com>
2020-03-06 14:13:27 -0800

    2020-03-06

--- Sources/Class/2020-03-05/CourseSchedule1.swift
+++ Sources/Class/2020-03-05/CourseSchedule1.swift
@@ -1,89 +1,15 @@
 import Foundation
 
-public final class Queue<E> : Sequence {
-    private var first: Node<E>? = nil
-    private var last: Node<E>? = nil
-    private(set) var count: Int = 0
-    fileprivate class Node<E> {
-        fileprivate var item: E
-        fileprivate var next: Node<E>?
-        fileprivate init(item: E, next: Node<E>? = nil) {
-            self.item = item
-            self.next = next
-        }
-    }
-    public init() {}
-    public func isEmpty() -> Bool {
-        return first == nil
-    }
-    public func peek() -> E? {
-        return first?.item
-    }
-    public func enqueue(item: E) {
-        let oldLast = last
-        last = Node<E>(item: item)
-        if isEmpty() { first = last }
-        else { oldLast?.next = last }
-        count += 1
-    }
-    public func dequeue() -> E? {
-        if let item = first?.item {
-            first = first?.next
-            count -= 1
-            if isEmpty() { last = nil }
-            return item
-        }
-        return nil
-    }
-    public struct QueueIterator<E> : IteratorProtocol {
-        private var current: Node<E>?
-        fileprivate init(_ first: Node<E>?) {
-            self.current = first
-        }
-        public mutating func next() -> E? {
-            if let item = current?.item {
-                current = current?.next
-                return item
-            }
-            return nil
-        }
-        public typealias Element = E
-    }
-    public __consuming func makeIterator() -> QueueIterator<E> {
-        return QueueIterator<E>(first)
-    }
-}
-extension Queue: CustomStringConvertible {
-    public var description: String {
-        return self.reduce(into: "") { $0 += "\($1) " }
-    }
-}
-
 class Solution {
-    func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
-        var output = [Int]()
-        var adj = [[Int]](repeating: [Int](), count: numCourses)
-        var indegree = [Int](repeating: 0, count: numCourses)
-        for req in prerequisites{
-            adj[req[1]].append(req[0])
-            indegree[req[0]] += 1
-        }
-        let q = Queue<Int>()
-        for v in 0..<numCourses{
-            if indegree[v]==0{
-                q.enqueue(item: v)
+    func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+        var vertexes = [Bool](repeating: false, count: numCourses)
+        for req in prerequisites {
+            if vertexes[req[0]] && vertexes[req[1]] {
+                return false
             }
+            vertexes[req[0]] = true
+            vertexes[req[1]] = true
         }
-        while !q.isEmpty() {
-            let x = q.dequeue()!
-            for v in adj[x] {
-                indegree[v] -= 1
-                if indegree[v] == 0{
-                    q.enqueue(item: v)
-                }
-            }
-            output.append(x)
-        }
-        return output
+        return true
     }
 }
--- Sources/Class/2020-03-05/CourseSchedule2.swift
+++ Sources/Class/2020-03-05/CourseSchedule2.swift
@@ -1,34 +1,35 @@
 import Foundation
 
-func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
-    var output = [Int]()
-
-    var adj = [[Int]]()
-    var indegree = [Int]()
-
-    for req in prerequisites{
-        adj[req[0]].append(req[1])
-        indegree[req[1]] += 1
-    }
-
-    //topological sort
-    let q = Queue<Int>()
-    // initial state
-    for v in 0..<numCourses{
-        if indegree[v]==0{
-            q.enqueue(item: v)
+class Solution {
+    func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+        var output = [Int]()
+        var count = 0
+        var adj = [[Int]](repeating: [Int](), count: numCourses)
+        var indegree = [Int](repeating: 0, count: numCourses)
+        for req in prerequisites{
+            adj[req[1]].append(req[0])
+            indegree[req[0]] += 1
         }
-    }
-
-    while !q.isEmpty() {
-        let x = q.dequeue()!
-        for v in adj[x] {
-            indegree[v] -= 1
-            if indegree[v] == 0{
+        let q = Queue<Int>()
+        for v in 0..<numCourses{
+            if indegree[v]==0{
                 q.enqueue(item: v)
             }
         }
-        output.append(x)
+        while !q.isEmpty() {
+            let x = q.dequeue()!
+            for v in adj[x] {
+                indegree[v] -= 1
+                if indegree[v] == 0{
+                    q.enqueue(item: v)
+                }
+            }
+            count += 1
+            output.append(x)
+        }
+        if count != numCourses {
+            output = []
+        }
+        return output
     }
-    return output
 }
